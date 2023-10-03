a62bb7c6f44a1a9cc57a23cd9f094f0e49a19c5c
Marc Vancells <vancellsmarc@gmail.com>
2023-10-03 11:54:49 +0200

    versió 1

--- p1.sh
+++ p1.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+read -p "Introdueix una opció: " x
+case $x in 
+	'q')
+		echo "Sortint de l'aplicació"
+		exit 10
+		;;
+	'lp')
+		cut -d',' -f7,8 cities.csv | uniq
+		;;
+	'sc')
+		cadena='(("\w+(+\w+)+"|\w+)'
+		read pais
+		egrep -cadena cities.csv | uniq
+		;;	
+esac
