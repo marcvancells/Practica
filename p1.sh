af096223af292492a75fd0d4848b5139f1d2818f
Marc Vancells <vancellsmarc@gmail.com>
2023-10-09 18:44:38 +0200

    Versió on ex3 funciona

--- p1.sh
+++ p1.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-codi_p='xx'
 
 read -p "Introdueix una opció: " x
-case $x in 
-	'q')
-		echo "Sortint de l'aplicació"
-		exit 10
-		;;
-	'lp')
-		cut -d',' -f7,8 cities.csv | uniq
-		;;
-	'sc') 
-		cadena='(("\w+( +\w+)+")|\w+)'
-		read -p "Introdueix un pais: " pais
-		echo $pais
-		codi_p= $(grep -E  '\w+,'"${cadena}"',\w+,\w+,'"${cadena}"', \w+,\w+,' $"pais" cities.csv | cut -d',' -f7 | uniq )
-		#if [[ -n codi_p ]]; then
-			#codi_p='xx'	
-		#else
-			#codi_p='xx'
-		#fi
-		;;
+case $x in
+
+    'q')
+        echo "Sortint de l'aplicació"
+        exit 10
+        ;;
+
+    'lp')
+        cut -d',' -f7,8 cities.csv | uniq
+        ;;
+
+    'sc')
+	cadena='(("\w+( +\w+)+")|\w+)'
+        read -p "Introdueix un pais: " resposta
+	YY=$(awk -F',' -v pais="$resposta" '$8 == pais {print $7}' cities.csv | sort | uniq)
+	if [ -n "$YY" ]; then
+		echo "$YY"
+	else
+		YY="XX"
+		echo "YY"
+	fi
+        ;;
 esac
+
