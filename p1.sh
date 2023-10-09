0c1227b3e20459dbdf0dfaccd5421fa780448dc1
Joan <joanbarba15@gmail.com>
2023-10-09 11:31:48 +0200

    versio3

--- p1.sh
+++ p1.sh
@@ -10,10 +10,15 @@ case $x in
 	'lp')
 		cut -d',' -f7,8 cities.csv | uniq
 		;;
-	'sc') #NO
+	'sc') 
 		cadena='(("\w+( +\w+)+")|\w+)'
 		read -p "Introdueix un pais: " pais
-		codi_p=$(grep -E  '$'pais' ^\w+, $(cadena), \w+, \w+, $(cadena), \w+, \w+, $(cadena)' cities.csv | cut -d',' -f7,8 cities.csv | uniq )	
+		echo $pais
+		codi_p= $(grep -E  '\w+,'"${cadena}"',\w+,\w+,'"${cadena}"', \w+,\w+,' $"pais" cities.csv | cut -d',' -f7 | uniq )
+		#if [[ -n codi_p ]]; then
+			#codi_p='xx'	
+		#else
+			#codi_p='xx'
+		#fi
 		;;
-	'se')
 esac
